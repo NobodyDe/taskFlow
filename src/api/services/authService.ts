6419fa35c1efea014220f8b522e4f70536d201f8
NobodyDe <henriquesantos.game@gmail.com>
2026-05-26 22:22:49 -0300

    refactor(AuthService): class static

--- src/api/services/authService.ts
+++ src/api/services/authService.ts
@@ -8,20 +8,28 @@ interface AuthResponse {
   access_token: string
 }
 
-export const authService = {
-  checkEmail: async (email: string): Promise<CheckEmailResponse> => {
+// export const authService = {
+//   checkEmail: async (email: string): Promise<CheckEmailResponse> => {
+//     const { data } = await api.post<CheckEmailResponse>('/auth/check-email', { email })
+//     return data
+//   },
+//   auth: async (email: string, password: string): Promise<AuthResponse> => {
+//     const { data } = await api.post<AuthResponse>('/auth/login', { email, password })
+//     return data
+//   },
+//   refreshToken: async (): Promise<AuthResponse> => {
+//     const { data } = await api.post<AuthResponse>('auth/refresh')
+//     return data
+//   },
+//   logout: async (): Promise<void> => {
+//     await api.post('/auth/logout')
+//   },
+// }
+
+export class AuthService {
+  static async checkEmail(email: string): Promise<CheckEmailResponse> {
     const { data } = await api.post<CheckEmailResponse>('/auth/check-email', { email })
+
     return data
-  },
-  auth: async (email: string, password: string): Promise<AuthResponse> => {
-    const { data } = await api.post<AuthResponse>('/auth/login', { email, password })
-    return data
-  },
-  refreshToken: async (): Promise<AuthResponse> => {
-    const { data } = await api.post<AuthResponse>('auth/refresh')
-    return data
-  },
-  logout: async (): Promise<void> => {
-    await api.post('/auth/logout')
-  },
+  }
 }
